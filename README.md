5d6cc5f8acabd3d382af14516ead7cd921118536
JeinJang <jein.jang@poomang.com>
2023-05-01 20:43:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# todo-list
+
+## 패키지 다운로드
+```
+npm i
+```
+
+## local 환경 실행
+```
+npm run dev
+```
+
+실행 후 http://localhost:3000으로 접속해주세요.
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Shooting coin</title>
+      </Head>
+      <div>
+        Hello
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
